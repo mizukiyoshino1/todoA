53ba768ba10d1ff97d6e248cb817359cc641f46e
mizukiyoshino1 <mizukiyoshino32@gmail.com>
2022-09-27 09:52:31 +0800

    modelsの作成

---
+++ app/Models/Bookmark.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Bookmark extends Model
+{
+    use HasFactory;
+
+    /*
+     リレーションの記載 
+    */
+
+    // Userとのリレーション
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    // Taskとのリレーション
+    public function task()
+    {
+        return $this->belongsTo('App\Task');
+    }
+}
